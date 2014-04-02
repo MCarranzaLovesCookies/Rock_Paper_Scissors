470b694739cd2983b02212a5d7e1d51cd7e40eea
MCarranzaLovesCookies <mike.carranza610@gmail.com>
2014-04-02 18:03:51 +0000

    We started working on a simplier R.P.S. game

---
+++ simplerps.rb
@@ -0,0 +1,4 @@
+OUTCOMES = {"rock" => {"rock" => "tie", "paper" => "player 2", "scissors" => "player 1"},
+  "paper" => {"paper" => "tie", "rock" => "player 1", "scissors" => "player 2"},
+  "scissors" => {"scissors" => "tie", "rock" => "player 2", "paper" => "player 2"}
+                            }
